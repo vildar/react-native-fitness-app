3ef88428aa87612907c0f4a4c471ab8fb278b811
Hanumanth P <48819979+vildar@users.noreply.github.com>
2020-06-14 18:25:18 +0530

    Added a button to reset state to 0

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,19 @@
 import React, {Component} from 'react'
-import {View} from 'react-native'
-import {getMetricMetaInfo} from '../utils/helpers'
+import {View, TouchableOpacity, Text} from 'react-native'
+import {getMetricMetaInfo, timeToString} from '../utils/helpers'
 import Slider from './Slider'
 import Stepper from './Stepper'
 import DateHeader from './DateHeader'
+
+function SubmitBtn({onPress}){
+    return (
+        <TouchableOpacity
+            onPress={onPress}
+        >
+            <Text>SUBMIT</Text>
+        </TouchableOpacity>
+    )
+}
 
 export default class AddEntry extends Component {
     state = {
@@ -12,6 +22,19 @@ export default class AddEntry extends Component {
         swim: 0,
         sleep: 0,
         eat: 0,
+    }
+
+    submit = () => {
+        const key = timeToString()
+        const entry = this.state
+
+        this.setState(() => ({
+            run: 0,
+            bike: 0,
+            swim: 0,
+            sleep: 0,
+            eat: 0,
+        }))
     }
 
     increment = (metric) => {
@@ -76,6 +99,7 @@ export default class AddEntry extends Component {
                         )
                     })
                 }
+                <SubmitBtn onPress={this.submit}/>
             </View>
         )
     }
