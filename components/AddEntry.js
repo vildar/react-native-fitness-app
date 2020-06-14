08c29f242c9ca2b3319de8f989af7ba88056a52f
Hanumanth P <48819979+vildar@users.noreply.github.com>
2020-06-14 17:18:36 +0530

    Added basic slider and stepper components

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import {View} from 'react-native'
 import {getMetricMetaInfo} from '../utils/helpers'
+import Slider from './Slider'
+import Stepper from './Stepper'
 
 export default class AddEntry extends Component {
     state = {
@@ -42,9 +44,36 @@ export default class AddEntry extends Component {
     }
 
     render(){
+        const metaInfo = getMetricMetaInfo()
+
         return (
             <View>
-                {getMetricMetaInfo('bike').getIcon()}
+                {
+                    Object.keys(metaInfo).map((key) => {
+                        const metric = metaInfo[key]
+                        const value = this.state[key]
+                        
+                        return (
+                            <View key={key}>
+                                {metric.getIcon()}
+                                {
+                                    metric.type === 'slider'
+                                    ?   <Slider
+                                            value = {value}
+                                            onChange = {(value) => this.slide(key, value)}
+                                            {...metric}
+                                        />
+                                    :   <Stepper
+                                            value = {value}
+                                            onIncrement = {() => this.increment(key)}
+                                            onDecrement = {() => this.decrement(key)}
+                                            {...metric}
+                                        />
+                                }
+                            </View>
+                        )
+                    })
+                }
             </View>
         )
     }
---
+++ components/Slider.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Text, View} from 'react-native'
+
+export default function Slider(){
+    return(
+        <View>
+            <Text>Slider</Text>
+        </View>
+    )
+}
