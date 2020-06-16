09c2ef8d3735ed13281b6c823eb74f8bfb2fb064
Hanumanth P <48819979+vildar@users.noreply.github.com>
2020-06-16 17:31:21 +0530

    Added Navigators

--- components/History.js
+++ components/History.js
@@ -7,8 +7,14 @@ import {fetchCalendarResults} from '../utils/api'
 import UdaciFitnessCalendar from 'udacifitness-calendar'
 import {white} from '../utils/colors'
 import DateHeader from './DateHeader'
+import MetricCard from './MetricCard'
+import { AppLoading} from 'expo'
 
 class History extends Component{
+    state = {
+        ready: false,
+    }
+
     componentDidMount(){
         const {dispatch} = this.props
 
@@ -21,6 +27,7 @@ class History extends Component{
                     }))
                 }
             })
+            .then(() => this.setState(() => ({ready: true})))
     }
 
     renderItem = ({today, ...metrics}, formattedDate, key) => (
@@ -34,7 +41,7 @@ class History extends Component{
                         </Text>
                     </View>
                 :   <TouchableOpacity onPress={() => console.log('Pressed!')}>
-                        <Text>{JSON.stringify(metrics)}</Text>
+                        <MetricCard date={formattedDate} metrics={metrics} />
                     </TouchableOpacity>
             }
         </View>
@@ -51,6 +58,12 @@ class History extends Component{
 
     render(){
         const {entries} = this.props
+
+        const { ready } = this.state
+
+        if (ready === false) {
+            return <AppLoading />
+        }
 
         return(
             <UdaciFitnessCalendar 
